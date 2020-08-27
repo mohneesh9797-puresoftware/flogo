e65f2c92728f80fee8a445356823ca8e73221276
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-08-27 14:55:17 +0530

    Update build-docs.sh

--- build-docs.sh
+++ build-docs.sh
@@ -9,12 +9,13 @@ HUGO_VERSION=0.74.3
 
 #--- Download and install prerequisites ---
 prerequisites() {
-    Pruthvi, 14:21
     if [ `uname -m` = 'aarch64' ]; then
             HUGO_ARCH = "ARM64"
             mkdir $HOME/gopath/bin
+            echo $HUGO_ARCH
     else
             HUGO_ARCH = "64bit"
+            echo $HUGO_ARCH
     fi
     wget -O hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-${HUGO_ARCH}.tar.gz
     mkdir -p hugobin
